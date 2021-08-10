9cce0e945b20d1be582ed26fdfe6af89789aa74c
ilaydasavaser <ilayda.5712@gmail.com>
2021-08-10 12:16:08 +0300

    grid view

--- lib/views/gridViewPage.dart
+++ lib/views/gridViewPage.dart
@@ -9,10 +9,37 @@ class _GridViewPageState extends State<GridViewPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Grid Page"),
-      ),
-      body: Container(),
-    );
+        appBar: AppBar(
+          title: Text("Grid Page"),
+        ),
+        body: Scrollbar(
+          // controller: _controller,
+          isAlwaysShown: true,
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: GridView.builder(
+              // controller: _controller,
+              itemCount: 50,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                mainAxisSpacing: 2,
+                crossAxisSpacing: 20,
+                crossAxisCount: 3,
+              ),
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  decoration: BoxDecoration(
+                    color: Colors.cyan,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Center(
+                      child: Text(
+                    '${index + 1}. lesson',
+                    style: TextStyle(color: Colors.white),
+                  )),
+                );
+              },
+            ),
+          ),
+        ));
   }
 }
